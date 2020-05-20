d92d0af590c71048e388d80711d4d9e6677413e8
Alberto Gomez <alberto.gomez@est.tech>
2020-05-20 13:02:35 +0200

    Update Geode version due to change in serialization

--- geode-core/src/main/java/org/apache/geode/internal/cache/wan/GatewaySenderEventImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/wan/GatewaySenderEventImpl.java
@@ -704,7 +704,7 @@ public class GatewaySenderEventImpl
   @Override
   public void toData(DataOutput out,
       SerializationContext context) throws IOException {
-    toDataPre_GEODE_1_12_0_0(out, context);
+    toDataPre_ERICSSONGEODE_1_12_0_1(out, context);
     boolean hasTransaction = this.transactionId != null;
     DataSerializer.writeBoolean(hasTransaction, out);
     if (hasTransaction) {
@@ -713,7 +713,7 @@ public class GatewaySenderEventImpl
     }
   }
 
-  public void toDataPre_GEODE_1_12_0_0(DataOutput out,
+  public void toDataPre_ERICSSONGEODE_1_12_0_1(DataOutput out,
       SerializationContext context) throws IOException {
     toDataPre_GEODE_1_9_0_0(out, context);
     DataSerializer.writeBoolean(this.isConcurrencyConflict, out);
@@ -748,8 +748,8 @@ public class GatewaySenderEventImpl
   @Override
   public void fromData(DataInput in,
       DeserializationContext context) throws IOException, ClassNotFoundException {
-    fromDataPre_GEODE_1_12_0_0(in, context);
-    if (version >= Version.GEODE_1_12_0.ordinal()) {
+    fromDataPre_ERICSSONGEODE_1_12_0_1(in, context);
+    if (version >= Version.ERICSSONGEODE_1_12_0_1.ordinal()) {
       boolean hasTransaction = DataSerializer.readBoolean(in);
       if (hasTransaction) {
         this.isLastEventInTransaction = DataSerializer.readBoolean(in);
@@ -758,7 +758,7 @@ public class GatewaySenderEventImpl
     }
   }
 
-  public void fromDataPre_GEODE_1_12_0_0(DataInput in, DeserializationContext context)
+  public void fromDataPre_ERICSSONGEODE_1_12_0_1(DataInput in, DeserializationContext context)
       throws IOException, ClassNotFoundException {
     fromDataPre_GEODE_1_9_0_0(in, context);
     if (version >= Version.GEODE_1_9_0.ordinal()) {
@@ -1281,7 +1281,7 @@ public class GatewaySenderEventImpl
 
   @Override
   public Version[] getSerializationVersions() {
-    return new Version[] {Version.GEODE_1_9_0, Version.GEODE_1_12_0};
+    return new Version[] {Version.GEODE_1_9_0, Version.ERICSSONGEODE_1_12_0_1};
   }
 
   public int getSerializedValueSize() {
